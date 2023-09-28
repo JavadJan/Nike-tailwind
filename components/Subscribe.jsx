836820a0b453b27fe21477c8a3a4645231a5e7e2
JavadJan <92234079+JavadJan@users.noreply.github.com>
2023-09-28 19:05:44 +0200

    done

--- components/Subscribe.jsx
+++ components/Subscribe.jsx
@@ -2,7 +2,7 @@ import Button from "./Button"
 
 const Subscribe = () => {
   return (
-    <section className="flex flex-col items-center justify-center gap-10">
+    <section id="contact-us" className="flex flex-col items-center justify-center gap-10">
       <h3 className="capitalize text-4xl font-palanquin font-bold">sign up for <span className="text-coral-red">update</span> & newsletter</h3>
       <div className="border border-slate-gray lg:max-w-[40%] w-full flex flex-row items-center justify-between max-sm:gap-2 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="Subscribe@gmail.com"
